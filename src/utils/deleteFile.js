b9bb7e2d9fd5b9b33fc86cc615554913b7c69850
Denny Wahyu <janexmgd@gmail.com>
2022-06-04 14:25:18 +0700

    remove console.log at /utils/deleteFile

--- src/utils/deleteFile.js
+++ src/utils/deleteFile.js
@@ -4,9 +4,6 @@ module.exports = (path) => {
   // cek apakah file exist
   if (fs.existsSync(path)) {
     // delete file
-    console.log("hello world");
     fs.unlinkSync(path);
-  } else {
-    console.log("failed delete file");
   }
 };
